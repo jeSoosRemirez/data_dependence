8520fad38d6a6d2790e459042876be2d9ab9b2be
jeSoosRemirez <dkhomych0091@gmail.com>
2024-06-05 01:58:49 +0300

    Format code via ruff

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,14 +3,14 @@ from utils.read_data import load_data
 
 
 en_files = [
-    os.path.join('data_files/en_files', file)
-    for file in os.listdir('data_files/en_files')
-    if file.endswith('.csv')
-    ]
+    os.path.join("data_files/en_files", file)
+    for file in os.listdir("data_files/en_files")
+    if file.endswith(".csv")
+]
 gp_files = [
-    os.path.join('data_files/gm_files', file)
-    for file in os.listdir('data_files/gm_files')
-    if file.endswith('.csv')
-    ]
+    os.path.join("data_files/gm_files", file)
+    for file in os.listdir("data_files/gm_files")
+    if file.endswith(".csv")
+]
 
 en_df, gp_df = load_data(en_files, gp_files)
